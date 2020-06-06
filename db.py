6a475ea29bef25c6f94a53626899c87f82da49f8
AbhaySinghBaloria <ugs17061_cse.abhay@cbit.org.in>
2020-06-06 09:43:12 +0530

    adding all

---
+++ db.py
@@ -0,0 +1,33 @@
+import pymysql
+from decimal import Decimal
+from flask import jsonify
+
+def query(querystr,return_json=True):
+    connection=pymysql.connect(host='localhost',
+                user='root',
+                password='',
+                db='test_api',
+                cursorclass=pymysql.cursors.DictCursor )
+    connection.begin()
+    cursor=connection.cursor()
+    cursor.execute(querystr)
+    result=encode(cursor.fetchall())
+    connection.commit()
+    cursor.close()
+    connection.close()
+    if return_json:
+        return jsonify(result)
+    else:
+        return result
+
+def encode(data):
+    for row in data:
+        for key,value in row.items():
+            if isinstance(value,Decimal):
+                row[key]=str(value)
+    return data
+    
+    
+
+    
+    
